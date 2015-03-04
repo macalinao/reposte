26ec3feba85d1ed8bf1616654c5b4bc4e797598f
Ian Macalinao <me@ian.pw>
2015-03-03 23:22:28 -0600

    Get subreddit route working

--- reposte.py
+++ reposte.py
@@ -2,7 +2,15 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 @app.route('/')
 def index():
     return 'Hello world!'
+
+@app.route('/r/<subreddit>')
+def subreddit(subreddit):
+    return 'Subreddit %s' % subreddit
+
+if __name__ == '__main__':
+    app.run()
